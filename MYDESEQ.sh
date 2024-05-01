ff6c41708393efd04f7b63b652ba54275e2f0cac
Marcos Costa <mabreucosta@gmail.com>
2024-05-01 15:37:03 +0100

    First

---
+++ MYDESEQ.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Too complicated to use NZ's script and the contrast funciton does not
+# work because the coefficients are incorrect. 
+
+# Re-designing input so that only two conditions are analysed at a time
--- NZ/NZ_script.R
+++ NZ/NZ_script.R
@@ -1,3 +1,5 @@
+setwd("/home/mcosta/MarcosCosta/projects/MIR184/DESeq2/NZ_deseq2")
+
 suppressMessages(library(EnhancedVolcano))
 suppressMessages(library(tximeta))
 suppressMessages(library(biomaRt))
@@ -14,67 +16,106 @@ suppressMessages(library(optparse))
 suppressMessages(library(readr))
 suppressMessages(library(dplyr))
 
+#####
+# Create an OptionParser object
+parser <- OptionParser(usage = "Usage: %prog [options]")
 
-## command line options
-option_list <- list( 
-        # specify output directory
-        make_option(c('-o', '--outdir'), action='store', default=NA,
-        type='character', help='output directory'),
-        # choose first condition, also used as reference level
-        make_option(c('-a', '--conditionA'), action='store', default=NA, type='character', help='first condition for comparison, also used as reference level for DESeq2'),
-        # choose second condition for comparison
-        make_option(c('-b', '--conditionB'), action='store', default=NA, type='character', help='second condition to be compared against condition_a'),
-        # padj cut-off value - default is 0.05
-        make_option(c('-c', '--padjCut'), action='store', default=0.05,type='double', help='padj cut-off value'),
-        # volcano plot fold change (FC) cutoff - default is 0.5
-        make_option(c('-x', '--VfcCut'), action='store',default=0.5, type='double', help='Fold change cutoff for Volcano'),
-        # volcano plot pCutOff - default is 0.05
-        make_option(c('-y', '--VpCut'), action='store', default=0.05, type='double', help='p-val cutoff for Volcano plot')
-)
+# Define the options
+parser <- add_option(parser, "-a", "--condition1",
+                     dest = "condition1", type = "character",
+					 help = "Description of condition1")
 
-opt <- parse_args(OptionParser(option_list = option_list))
+parser <- add_option(parser, "-b", "--condition2",
+                     dest = "condition2", type = "character",
+					 help = "Description of condition2")
 
-if (!is.na(opt$outdir)) {
-  output_dir <- opt$outdir
-  label <- basename(opt$outdir)
-} else {
-  print("error: no output directory specified")
-}
+# Parse the command line arguments
+opt <- parse_args(parser)
 
-get
-# read in counts
-countData <- read.csv("/home/mcosta/projects/deseq2/interger_counts_data_MIR184.csv", row.names = 1)
+# Access the parsed options
+condition1 <- opt$condition1
+condition2 <- opt$condition2
 
+condition1 <- "scramble"
+condition2 <- "mir184"
+
+label <- paste(condition1, condition2, sep = "_")
+
+padjCut=0.05
+VfcCut=0.5
+VpCut=0.05
+#####
 # read-in sample metadata (all)
-colData <- read.csv("/home/mcosta/projects/deseq2/sample_info.csv", row.names = 1 )
+quants <- fread("sample_data.csv")
+
 # define conditions
-coldata <- countData[condition %in% c(opt$conditionA,
-                                   opt$conditionB)]
+coldata <- quants[condition %in% c(condition1, condition2)]
+coldata$condition <- factor(coldata$condition)
+coldata
 
-coldata <- countData[condition %in% c("scramble",
-                                      "mir184")]
+# read in counts
+countData <- read.csv("interger_counts_data_MIR184.csv", row.names = 1)
+head(countData)
+ncol(countData)
+# read in sample info
 
+coldata <- read.csv("sample_info.csv", row.names = 1 )
+head(coldata,10)
+nrow(coldata)
+# Making sure row names in coldata match and countData
+all(colnames(countData) %in% rownames(coldata))
+all(colnames(countData) == rownames(coldata))
 
-
-colData$condition <- factor(colData$condition)
+# Example code to convert a character variable to a factor
 coldata$condition <- factor(coldata$condition)
 
-## DESeq2 standard workflow
-dds <- DESeqDataSetFromMatrix(countData = countData,
-                              colData = colData,
-                              design= ~ condition)
-keep <- rowSums(counts(dds) >= 5) >= 4
-dds <- dds[keep,]
-dds$condition <- relevel(dds$condition, ref = opt$conditionA)
-dds <- DESeq(dds)
-res <- results(dds, filterFun=ihw, alpha = opt$padjCut) # MLE
+# Make matrix
+# Subset the count matrix to include only the selected conditions
+selected_counts <- counts[, colnames(counts) %in% selected_conditions]
 
-# shrinkage - generate MAP values for plotting and ranking only
+#Create the DESeqDataSet object
+dds <- DESeqDataSetFromMatrix(countData = selected_counts,
+                              coldata = coldata,
+                              design = ~ condition)
+
+dds <- DESeqDataSetFromMatrix(countData = countData,
+                              coldata = coldata,
+                              design= ~ condition )
+
+
+dds
+# Reduce dimensions and eliminate unnecessary analysis
+smallestGroupSize <- 3
+keep <- rowSums(counts(dds) >= 10) >= smallestGroupSize
+dds <- dds[keep,] # Dimension reduced from 63241x24 to 18923x24
+print("Dimensions reduced")
+dds
+
+# Define a vector of conditions you want to include
+selected_conditions <- c(condition1, condition2)
+
+# Subset the dds object to include only the selected conditions
+dds_selected <- dds[, coldata(dds)$condition %in% selected_conditions]
+dds_selected
+# Now you can run DESeq on the subsetted dds object
+dds_results <- DESeq(dds_selected)
+
+# After running DESeq, you can retrieve the results
+results <- results(dds_results)
+
+# Run DESeq2
+
+dds <- DESeq(dds)
+dds$condition <- relevel(dds$condition, ref = "scramble")
+resultsNames(dds) # lists the coefficients
+res <- results(dds, contrast=c("condition",condition1,condition2))
+res
+# srhinkage - generate MAP values for plotting and ranking only
 coef <- resultsNames(dds)[2]
 print(coef)
 resLFC <- lfcShrink(dds, coef=coef, type="apeglm") # MAP
 
-pdf(paste(output_dir, label, '_MA.pdf', sep =''))
+pdf(paste(label,"MA.pdf", sep ='_'))
 plotMA(resLFC, ylim=c(-2,2))
 dev.off()
 
@@ -105,10 +146,10 @@ plot_lfc <- merge(resLFC_dt,name.p, by.x='geneID', by.y="ensembl_gene_id", sort 
 
 # sort and subset significant events, write to csv
 resOrdered <- res_named[order(abs(res_named$log2FoldChange.MAP), decreasing=TRUE),]
-fwrite(resOrdered, paste(output_dir, label, '_deseq2_results_ihw_no_filter.csv', sep =''))
+fwrite(resOrdered, paste(label, 'deseq2_results_ihw_no_filter.csv', sep ='_'))
 
-resSig <- subset(resOrdered, padj < as.numeric(opt$padjCut))
-fwrite(resSig, paste(output_dir, label, '_deseq2_results_ihw_padj_filtered.csv', sep =''))
+resSig <- subset(resOrdered, padj < as.numeric(padjCut))
+fwrite(resSig, paste(label, '_deseq2_results_ihw_padj_filtered.csv', sep ='_'))
 
 print('results written')
 
@@ -125,25 +166,25 @@ vsd_pca <- prcomp(vsd_matrix) # transpose so that PC is calculated for genes ins
 load <- as.data.frame(vsd_pca$rotation)
 pc1 <- load[order(load$PC1, decreasing = TRUE),]
 pc1 <- subset(pc1, select = PC1)
-fwrite(pc1, paste(output_dir, label, '_pc1_loading_genes.csv', sep =''), row.names = TRUE)
+fwrite(pc1, paste(label, '_pc1_loading_genes.csv', sep ='_'), row.names = TRUE)
 
 pc2 <- load[order(load$PC2, decreasing = TRUE),]
 pc2 <- subset(pc2, select = PC2)
-fwrite(pc2, paste(output_dir, label, '_pc2_loading_genes.csv', sep =''), row.names = TRUE)
+fwrite(pc2, paste(label, 'pc2_loading_genes.csv', sep ='_'), row.names = TRUE)
 
 # PCA
 title <- strsplit(coef, '_')[[1]]
 
-svg(paste(output_dir, label, '_pca.svg', sep =''))
+svg(paste(label, '_pca.svg', sep ='_'))
 pcaData <- plotPCA(vsd, returnData=TRUE)
 percentVar <- round(100 * attr(pcaData, "percentVar"))
-ggplot(pcaData, aes(PC1, PC2, color=condition)) + geom_point(size=1) + xlab(paste0("PC1: ",percentVar[1],"% variance")) + ylab(paste0("PC2: ",percentVar[2],"% variance")) + geom_text_repel(label = coldata$names) + labs(title = paste(toupper(title[2]), 'vs', toupper(title[4]), 'with DESeq2')) + coord_fixed() + theme_bw()
+ggplot(pcaData, aes(PC1, PC2, color=condition)) + geom_point(size=1) + xlab(paste0("PC1: ",percentVar[1],"% variance")) + ylab(paste0("PC2: ",percentVar[2],"% variance")) + geom_text_repel(label = coldata$condition) + labs(title = paste(toupper(title[2]), 'vs', toupper(title[4]), 'with DESeq2')) + coord_fixed() + theme_bw()
 dev.off()
 print('PCA plot saved')
 
 # # volcano plot
-svg(paste(output_dir, label, '_volcano.svg', sep =''))
-EnhancedVolcano(plot_lfc, lab = plot_lfc$hgnc_symbol, x = 'log2FoldChange', y = 'padj', ylab = expression(-log[10]~padj), legendPosition = 'none', drawConnectors = TRUE, max.overlaps = 7, title = paste(toupper(title[2]), 'vs', toupper(title[4])), pCutoff = as.numeric(opt$VpCut), FCcutoff = as.numeric(opt$VfcCut))
+svg(paste(label, '_volcano.svg', sep =''))
+EnhancedVolcano(plot_lfc, lab = plot_lfc$hgnc_symbol, x = 'log2FoldChange', y = 'padj', ylab = expression(-log[10]~padj), legendPosition = 'none', drawConnectors = TRUE, max.overlaps = 7, title = paste(toupper(title[2]), 'vs', toupper(title[4])), pCutoff = as.numeric(VpCut), FCcutoff = as.numeric(VfcCut))
 dev.off()
 print('Volcano plot saved')
 
@@ -168,13 +209,13 @@ for (i in ont) {
     gse <- gseGO(geneList = genelist, OrgDb = org.Hs.eg.db, ont = i, minGSSize = 10, maxGSSize = 500, pvalueCutoff = 1, verbose = FALSE, nPermSimple = 10000)
 
     if (nrow(go) > 0) {
-        fwrite(as.data.frame(go), paste(output_dir, label, '_go_enrichment_', i,'.csv', sep =''))
+        fwrite(as.data.frame(go), paste(label, '_go_enrichment_', i,'.csv', sep =''))
     } else {
         print('no GO enrichment')
     }
 
     if (nrow(gse) > 0) {
-        fwrite(as.data.frame(gse), paste(output_dir, label, '_go_gsea_enrichment_', i,'.csv', sep =''))
+        fwrite(as.data.frame(gse), paste(label, '_go_gsea_enrichment_', i,'.csv', sep =''))
     } else {
         print('no GSEA enrichment')
     }
@@ -183,7 +224,7 @@ for (i in ont) {
 rpa_path <- enrichPathway(gene = gene, organism = "human", pAdjustMethod = 'BH', pvalueCutoff = 1, qvalueCutoff = 1, readable = TRUE)
 
 if (nrow(rpa_path) > 0) {
-        fwrite(as.data.frame(rpa_path), paste(output_dir, label, '_reactomePA_enrichment.csv', sep =''))
+        fwrite(as.data.frame(rpa_path), paste(label, '_reactomePA_enrichment.csv', sep =''))
     } else {
         print('no reactome enrichment')
 }
@@ -192,7 +233,7 @@ if (nrow(rpa_path) > 0) {
 wiki_path <- enrichWP(gene = gene, pAdjustMethod = 'BH', pvalueCutoff = 1, qvalueCutoff = 1, organism = "Homo sapiens") 
 
 if (nrow(wiki_path) > 0) {
-        fwrite(as.data.frame(wiki_path), paste(output_dir, label, '_wikipaths_enrichment.csv', sep =''))
+        fwrite(as.data.frame(wiki_path), paste(label, '_wikipaths_enrichment.csv', sep =''))
     } else {
         print('no wiki enrichment')
 }
@@ -205,20 +246,20 @@ mkegg <- enrichMKEGG(gene = gene, organism = 'hsa', pvalueCutoff = 1, pAdjustMet
 kegg_gsea <- gseKEGG(geneList = genelist, organism = 'hsa', minGSSize = 10, pvalueCutoff = 1, pAdjustMethod = "BH", verbose = FALSE, nPermSimple = 10000)
 
 if (nrow(kegg) > 0) {
-        fwrite(as.data.frame(setReadable(kegg, OrgDb = org.Hs.eg.db, keyType="ENTREZID")), paste(output_dir, label, '_kegg_enrichment.csv', sep =''))
+        fwrite(as.data.frame(setReadable(kegg, OrgDb = org.Hs.eg.db, keyType="ENTREZID")), paste(label, '_kegg_enrichment.csv', sep =''))
     } else {
         print('no KEGG enrichment')
     }
 
 if (nrow(as.data.frame(mkegg)) > 0) {
-        fwrite(as.data.frame(setReadable(mkegg, OrgDb = org.Hs.eg.db, keyType="ENTREZID")), paste(output_dir, label, '_kegg_module_enrichment.csv', sep =''))
+        fwrite(as.data.frame(setReadable(mkegg, OrgDb = org.Hs.eg.db, keyType="ENTREZID")), paste(label, '_kegg_module_enrichment.csv', sep =''))
     } else {
         print('no KEGG module enrichment')
     }
 
 
 if (nrow(kegg_gsea) > 0) {
-    fwrite(as.data.frame(setReadable(kegg_gsea, OrgDb = org.Hs.eg.db, keyType="ENTREZID")), paste(output_dir, label, '_kegg_gsea_enrichment.csv', sep =''))
+    fwrite(as.data.frame(setReadable(kegg_gsea, OrgDb = org.Hs.eg.db, keyType="ENTREZID")), paste(label, '_kegg_gsea_enrichment.csv', sep =''))
 } else {
     print('no KEGG GSEA enrichment')
 }
