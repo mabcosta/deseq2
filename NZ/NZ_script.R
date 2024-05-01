4dc724eaaeb617036016424a0f50878f603a8157
Marcos Costa <mabreucosta@gmail.com>
2024-05-01 16:45:06 +0100

    Reference condition added

--- NZ/NZ_script.R
+++ NZ/NZ_script.R
@@ -1,5 +1,5 @@
-setwd("/home/mcosta/MarcosCosta/projects/MIR184/DESeq2/NZ_deseq2")
-
+setwd("/home/mcosta/projects/deseq2/NZ")
+suppressMessages(library(apeglm))
 suppressMessages(library(EnhancedVolcano))
 suppressMessages(library(tximeta))
 suppressMessages(library(biomaRt))
@@ -7,125 +7,105 @@ suppressMessages(library(IHW))
 suppressMessages(library(clusterProfiler))
 suppressMessages(library(org.Hs.eg.db))
 suppressMessages(library(ReactomePA))
-suppressMessages(library(data.table))
 suppressMessages(library(ggplot2))
-suppressMessages(library(DESeq2))
-suppressMessages(library(apeglm))
 suppressMessages(library(ggfortify))
-suppressMessages(library(optparse))
 suppressMessages(library(readr))
 suppressMessages(library(dplyr))
+suppressMessages(library(optparse)) # Parsing package
 
-#####
+############################################################################
 # Create an OptionParser object
 parser <- OptionParser(usage = "Usage: %prog [options]")
 
 # Define the options
-parser <- add_option(parser, "-a", "--condition1",
-                     dest = "condition1", type = "character",
-					 help = "Description of condition1")
+parser <- add_option(parser, "-a", "--condition",
+                     dest = "condition", type = "character",
+					 help = "Description of condition")
 
-parser <- add_option(parser, "-b", "--condition2",
-                     dest = "condition2", type = "character",
-					 help = "Description of condition2")
+parser <- add_option(parser, "-r", "--reference",
+                     dest = "reference", type = "character",
+					 help = "Reference for DESeq2 run")
 
 # Parse the command line arguments
 opt <- parse_args(parser)
 
 # Access the parsed options
-condition1 <- opt$condition1
-condition2 <- opt$condition2
+condition <- opt$condition
+ref_condition <- opt$reference
 
-condition1 <- "scramble"
+condition <- "scramble"
 condition2 <- "mir184"
+ref_condition <- "scramble"
 
-label <- paste(condition1, condition2, sep = "_")
+label <- paste(condition, condition2, sep = "_")
 
-padjCut=0.05
-VfcCut=0.5
-VpCut=0.05
-#####
-# read-in sample metadata (all)
-quants <- fread("sample_data.csv")
+padj_Cut <- 0.05
+Vfc_Cut <- 0.5
+Vp_Cut <- 0.05
 
-# define conditions
-coldata <- quants[condition %in% c(condition1, condition2)]
-coldata$condition <- factor(coldata$condition)
-coldata
-
-# read in counts
+############################################################################
+### Get data
 countData <- read.csv("interger_counts_data_MIR184.csv", row.names = 1)
-head(countData)
-ncol(countData)
-# read in sample info
+colData <- read.csv("sample_info.csv", row.names = 1 )
 
-coldata <- read.csv("sample_info.csv", row.names = 1 )
-head(coldata,10)
-nrow(coldata)
-# Making sure row names in coldata match and countData
-all(colnames(countData) %in% rownames(coldata))
-all(colnames(countData) == rownames(coldata))
+# Making sure row names in colData match and are
+# in the same order as counts_data
+all(colnames(countData) %in% rownames(colData))
+all(colnames(countData) == rownames(colData))
 
-# Example code to convert a character variable to a factor
-coldata$condition <- factor(coldata$condition)
-
-# Make matrix
-# Subset the count matrix to include only the selected conditions
-selected_counts <- counts[, colnames(counts) %in% selected_conditions]
-
-#Create the DESeqDataSet object
-dds <- DESeqDataSetFromMatrix(countData = selected_counts,
-                              coldata = coldata,
-                              design = ~ condition)
+### Create the DESeqDataSet object
+suppressMessages(library(DESeq2))
 
 dds <- DESeqDataSetFromMatrix(countData = countData,
-                              coldata = coldata,
+                              colData = colData,
                               design= ~ condition )
+### Changing reference order
+levels(dds$condition) # Default is ordered alphabetically
+print("Changing the level order...")
 
+dds$condition <- relevel(dds$condition, ref = ref_condition)
 
-dds
-# Reduce dimensions and eliminate unnecessary analysis
+levels(dds$condition)
+
+### Reduce dimensions and eliminate unnecessary analysis
+print("Initial dimensions:")
+summary(dds)
+
 smallestGroupSize <- 3
 keep <- rowSums(counts(dds) >= 10) >= smallestGroupSize
-dds <- dds[keep,] # Dimension reduced from 63241x24 to 18923x24
+dds <- dds[keep,] # Dimension reduced here
+
 print("Dimensions reduced")
-dds
+summary(dds)
 
-# Define a vector of conditions you want to include
-selected_conditions <- c(condition1, condition2)
-
-# Subset the dds object to include only the selected conditions
-dds_selected <- dds[, coldata(dds)$condition %in% selected_conditions]
-dds_selected
-# Now you can run DESeq on the subsetted dds object
-dds_results <- DESeq(dds_selected)
-
-# After running DESeq, you can retrieve the results
-results <- results(dds_results)
-
-# Run DESeq2
-
+############################################################################
+### Run DESeq2
 dds <- DESeq(dds)
-dds$condition <- relevel(dds$condition, ref = "scramble")
+
+# Get results
 resultsNames(dds) # lists the coefficients
-res <- results(dds, contrast=c("condition",condition1,condition2))
+res <- results(dds, contrast = c("condition", ref_condition, condition))
 res
-# srhinkage - generate MAP values for plotting and ranking only
+
+############################################################################
+### Shrinkage - generate MAP values for plotting and ranking only
 coef <- resultsNames(dds)[2]
 print(coef)
-resLFC <- lfcShrink(dds, coef=coef, type="apeglm") # MAP
+resLFC <- lfcShrink(dds, coef = coef, type = "apeglm") # MAP
 
+# Save plot
 pdf(paste(label,"MA.pdf", sep ='_'))
 plotMA(resLFC, ylim=c(-2,2))
 dev.off()
 
-# convert resLFC and res objects to data tables and Gencode IDs to Ensembl
+### convert resLFC and res objects to data tables and Gencode IDs to Ensembl
 resLFC$geneID <- gsub("\\..*","", rownames(resLFC))
 res$geneID <- gsub("\\..*","", rownames(resLFC))
 resLFC_dt <- as.data.table(resLFC)
 res_dt <- as.data.table(res)
 
-# merge MAP logfoldchange values to MLE table, rename the logfoldchange columns to .MLE and .MAP
+# merge MAP logfoldchange values to MLE table,
+# rename the logfoldchange columns to .MLE and .MAP
 map <- resLFC_dt[, .(log2FoldChange, geneID)] 
 res1 <- merge(res_dt, map, by = 'geneID') # final DEGs table
 setnames(res1, old=c("log2FoldChange.x","log2FoldChange.y"), new=c("log2FoldChange.MLE", "log2FoldChange.MAP"))
@@ -148,7 +128,7 @@ plot_lfc <- merge(resLFC_dt,name.p, by.x='geneID', by.y="ensembl_gene_id", sort 
 resOrdered <- res_named[order(abs(res_named$log2FoldChange.MAP), decreasing=TRUE),]
 fwrite(resOrdered, paste(label, 'deseq2_results_ihw_no_filter.csv', sep ='_'))
 
-resSig <- subset(resOrdered, padj < as.numeric(padjCut))
+resSig <- subset(resOrdered, padj < as.numeric(padj_Cut))
 fwrite(resSig, paste(label, '_deseq2_results_ihw_padj_filtered.csv', sep ='_'))
 
 print('results written')
@@ -184,7 +164,7 @@ print('PCA plot saved')
 
 # # volcano plot
 svg(paste(label, '_volcano.svg', sep =''))
-EnhancedVolcano(plot_lfc, lab = plot_lfc$hgnc_symbol, x = 'log2FoldChange', y = 'padj', ylab = expression(-log[10]~padj), legendPosition = 'none', drawConnectors = TRUE, max.overlaps = 7, title = paste(toupper(title[2]), 'vs', toupper(title[4])), pCutoff = as.numeric(VpCut), FCcutoff = as.numeric(VfcCut))
+EnhancedVolcano(plot_lfc, lab = plot_lfc$hgnc_symbol, x = 'log2FoldChange', y = 'padj', ylab = expression(-log[10]~padj), legendPosition = 'none', drawConnectors = TRUE, max.overlaps = 7, title = paste(toupper(title[2]), 'vs', toupper(title[4])), pCutoff = as.numeric(Vp_Cut), FCcutoff = as.numeric(Vfc_Cut))
 dev.off()
 print('Volcano plot saved')
 
